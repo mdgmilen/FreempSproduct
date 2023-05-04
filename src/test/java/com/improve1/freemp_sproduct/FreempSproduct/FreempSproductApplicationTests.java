a911bfa2f94dac11b4fcbab6da04196bc19c76bd
Milen Georgiev <milen.georgiev@improve1.com>
2023-05-04 18:24:00 +0300

    trainings in progress 1

---
+++ src/test/java/com/improve1/freemp_sproduct/FreempSproduct/FreempSproductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.improve1.freemp_sproduct.FreempSproduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreempSproductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
